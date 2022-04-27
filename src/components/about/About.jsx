9bde7822501adf31e13d7e604ec84f7ef7a702c6
erik1998serrano <erik1998serrano@outlook.com>
2022-04-28 00:41:56 +1000

    updated about section: added stacks

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,14 +6,54 @@ const About = () => {
             <div className="left">
                 <h2>About Me</h2>
                 <p>
-                    I'm very ambitious developer looking for a role in
-                    established IT company with the opportunity to work with the
-                    latest technologies on challenging and diverse projects.
+                    I'm a very ambitious developer looking for a role in a
+                    company which will provide me an opportunity to work with
+                    the latest technologies on challenging and diverse projects.
                 </p>
                 <p>
                     I'm quietly confident, naturally curious, and perpetually
                     working on improving my skills one problem at a time.
                 </p>
+                <h3>My Stacks</h3>
+                <div className="icons">
+                    <div class="one">
+                        <img src="assets/stacks/js.svg" alt="" />
+                    </div>
+                    <div class="two">
+                        <img src="assets/stacks/css.svg" alt="" />
+                    </div>
+                    <div class="three">
+                        <img src="assets/stacks/sass.svg" alt="" />
+                    </div>
+                    <div class="four">
+                        <img src="assets/stacks/html.svg" alt="" />
+                    </div>
+                    <div class="five">
+                        <img src="assets/stacks/git.svg" alt="" />
+                    </div>
+                    <div class="six">
+                        <img src="assets/stacks/java.svg" alt="" />
+                    </div>
+                    <div class="seven">
+                        <img src="assets/stacks/react.svg" alt="" />
+                    </div>
+                    <div class="eight">
+                        <img src="assets/stacks/springboot.svg" alt="" />
+                    </div>
+                    <div class="nine">
+                        <img src="assets/stacks/sql.svg" alt="" />
+                    </div>
+                    <div class="ten">
+                        <img src="assets/stacks/mysql.svg" alt="" />
+                    </div>
+                    <div class="eleven">
+                        <img src="assets/stacks/firestore.svg" alt="" />
+                    </div>
+                    <div class="twelve">
+                        <img src="assets/stacks/aws.svg" alt="" />
+                    </div>
+                </div>
+                <div className="icons"></div>
             </div>
 
             <div className="right">
